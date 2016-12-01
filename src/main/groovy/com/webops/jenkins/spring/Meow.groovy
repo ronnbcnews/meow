6953756fb0a89111126d0015ca18192aaa8a46d4
Ron Calderon <Ron.calderon@nbcuni.com>
2016-12-01 00:55:12 -0500

    test

--- src/main/groovy/com/webops/jenkins/spring/Meow.groovy
+++ src/main/groovy/com/webops/jenkins/spring/Meow.groovy
@@ -10,10 +10,10 @@ import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.autoconfigure.web.*
 import org.yaml.snakeyaml.Yaml
 
-@Configuration
+//@Configuration
 //@EnableAutoConfiguration(exclude = [ WebMvcAutoConfiguration.class, WebClientAutoConfiguration.class,
 //    HttpMessageConvertersAutoConfiguration.class, GroovyTemplateAutoConfiguration.class])
-@EnableConfigurationProperties(GitHubProperties.class)
+//@EnableConfigurationProperties(GitHubProperties.class)
 //@ComponentScan(['com.webops.jenkins.spring', 'com.webops.jenkins.spring.config'])
 public class Meow {
   final Map<String, String> DEFAULT_PROPS = [
