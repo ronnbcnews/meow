fb03f713b792de39eaa5eabc0b138b78bc6c0711
Ron Calderon <Ron.calderon@nbcuni.com>
2016-12-02 02:56:34 -0500

    test

--- src/main/groovy/com/webops/jenkins/spring/Meow.groovy
+++ src/main/groovy/com/webops/jenkins/spring/Meow.groovy
@@ -30,7 +30,7 @@ public class Meow {
 
   void main() {
     ApplicationContext context = new ClassPathXmlApplicationContext("classpath*:src/main/resources/ApplicationContext.xml");
-    GitHubConfig p1 = (GitHubConfig) context.getBean("gitHubConfig");
+    GitHubConfig p1 = (GitHubConfig) context.getBean('gitHubConfig');
     System.out.println(p1.main());
   }
 }
